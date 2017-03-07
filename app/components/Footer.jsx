5c60d84002ce9f33e46f6251a65dae185ee1e5b0
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-03-07 15:52:13 -0600

    working on footer hover over social media

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -7,7 +7,7 @@ class Footer extends Component {
 				<div className="container">
 					<div className="row">
 						<div className="text-center">
-							<p className="fh5co-social-icons">
+							<p className="social-icons">
                 <i className="fa fa-twitter fa-2x" aria-hidden="true"></i>
                 <i className="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
 								<a href="#"><i className="icon-twitter2"></i></a>
@@ -16,8 +16,8 @@ class Footer extends Component {
 								<a href="#"><i className="icon-dribbble2"></i></a>
 								<a href="#"><i className="icon-youtube"></i></a>
 							</p>
-							<p>
-                COPYRIGHT © 2014-2017 FOOTBALL-DATA.ORG.
+							<p className="footerwebsite">
+                COPYRIGHT © 2014-2017 <a href="http://football-data.org">FOOTBALL-DATA.ORG</a>.
 							</p>
               <p>
                 ALL RIGHTS RESERVED.
