f3304a5ecf78aa34d6551b969b5bfc018c2cfbc5
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-03-09 12:14:07 -0600

    next paragraph

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -21,6 +21,23 @@ class Main extends Component {
               <a className="apibutton" href="#" role="button">Request an API Key</a>
             </div>
           </div>
+          <div className="mainlandingparagraph">
+            <div className="paragraph">
+              <p>The goal of the football-data API is to provide football data of all major european leagues in a machine-readable way. By data I mean fixtures, teams, players, results and more. All this information is provided via an easy-to-use RESTful API in JSON (only) representation.</p>
+            </div>
+            <div className="paragraph">
+              <p>Target audience are software developers with an affinity to football in any way and the intent of building corresponding software.</p>
+            </div>
+            <div className="paragraph">
+              <p>The on-site documentation should get you going quickly. I suggest you to install a JSON-View-Plugin either for Firefox or Chrome and then dive in via the Competition Resource and follow the HAL-style links of each resource to traverse the API.</p>
+            </div>
+            <div className="paragraph">
+              <p>If you want to implement the API seriously in a (larger) project, read the exhaustive documentation to learn the tiny details.</p>
+            </div>
+            <div className="paragraph">
+              <p>The API is in active development and as the current version number finally allegorizes: stable. Follow the blog for up-to-date information.</p>
+            </div>
+          </div>
         <Footer />
       </div>
     )
